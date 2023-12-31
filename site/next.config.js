8d953d16a298a74b058e603e42f931ca9799f645
4e4eh <96462042+4e4eh@users.noreply.github.com>
2023-12-31 11:05:24 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'Kode\'s Registry',
     description: 'The my Kasm Workspaces.',
-    icon: 'https://github.com/4e4eh/kasm-registry/blob/1.0/site/public/pngwing.png',
+    icon: 'https://github.com/4e4eh/kasm-registry/pngwing.png',
     listUrl: 'https://4e4eh.github.io/kasm-registry/',
     contactUrl: 'https://github.com/4e4eh/kasm-registry/issues',
   },
